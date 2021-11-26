5e9d499469fad041c3aa858c1af9cbf39b0d29f0
马 思腾 <stma@120.pami.group>
2021-11-26 13:51:17 +0800

    update vgg backbone

---
+++ net/vgg_with_projector.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+import net.vgg as vgg
+from net.MLP_head import MLPHead
+
+
+class my_vgg_with_projector(nn.Module):
+    def __init__(self, hidden_channels, out_channels):
+        super(my_vgg_with_projector, self).__init__()
+        #self.resnet = models.resnet18(pretrained=False)
+        #self.encoder = nn.Sequential(*list(self.resnet.children())[:-1])
+        self.encoder = vgg.VGG_basemodel('kaiming').features
+        self.projector = MLPHead(512 * 6 * 6, hidden_channels, out_channels)
+
+    def forward(self, x):
+        temp = self.encoder(x)
+        #print(temp.shape)
+        temp = torch.flatten(temp, 1)
+        #temp = temp.view(temp.shape[0], temp.shape[1])
+        #print(temp.shape)
+        return self.projector(temp)
