6e0bcf16ad74ffa855f8f7b3d933b268750ebcc1
longzili <longzilipro@gmail.com>
2021-11-28 07:52:32 +0000

    <multi card> log： 	1.新建BYOLmodule 	2.多卡 next step: 	更新parser 	更新acc

--- train.py
+++ train.py
@@ -2,6 +2,7 @@ import torch
 from torch.functional import Tensor
 import torchvision
 from torch.utils.data import DataLoader, TensorDataset
+from torch.optim.lr_scheduler import StepLR
 import os
 import data_augmentation.data_transform as transform
 from data_augmentation.multiview_data import MultiviewData
@@ -9,12 +10,26 @@ from trainer import BYOLTrainer
 from net.resnet_with_projector import my_resnet_with_projector
 from net.vgg_with_projector import my_vgg_with_projector
 from net.MLP_head import MLPHead
+import argparse
+
 
 torch.manual_seed(0)
 
 
+def get_parser():
+    parser = argparse.ArgumentParser(description='BYOL_parser')
+    #DDP
+    parser.add_argument('--local_rank',default=-1,type=int,
+                        help='node rank for distributed training')
+    return parser
+
+
+
 def main():
-    device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
+    parser = get_parser()
+    opt = parser.parse_args()
+
+    # device = 'cuda:1' if torch.cuda.is_available() else 'cpu'
     data_root = "/mnt/pami23/longzili/DATA/nndl_BYOL"
     checkpoint_path = '/mnt/pami23/longzili/checkfolder/nndl_BYOL/'
     num_epoch = 40
@@ -34,20 +49,21 @@ def main():
         #transform=torchvision.transforms.ToTensor(),
         download=True)
 
-    online_net = my_vgg_with_projector(512, 128).to(device)
+    online_net = my_vgg_with_projector(512, 128)
     online_predictor = MLPHead(online_net.projector.layer[-1].out_features,
-                               512, 128).to(device)
-    target_net = my_vgg_with_projector(512, 128).to(device)
+                               512, 128)
+    target_net = my_vgg_with_projector(512, 128)
 
     optimizer = torch.optim.Adam(list(online_net.parameters()) +
                                  list(online_predictor.parameters()),
                                  lr,
                                  beta,
                                  weight_decay)
+    step_optimizer = StepLR(optimizer, step_size = 10, gamma=0.5)
     momentum_ori = 0.999
     my_trainer = BYOLTrainer(num_epoch, batch_size, online_net,
-                             online_predictor, target_net, optimizer,
-                             momentum_ori, device, checkpoint_path)
+                             online_predictor, target_net, optimizer, step_optimizer,
+                             momentum_ori, checkpoint_path,opt)
     my_trainer.train(data)
 
 
