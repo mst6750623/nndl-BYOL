f3cf65b314714a315c31509c84de37da59e81d9a
马 思腾 <stma@120.pami.group>
2021-11-28 17:16:10 +0800

    [inference update]

--- data_augmentation/cifar-transform.py
+++ data_augmentation/cifar-transform.py
@@ -6,7 +6,7 @@ from data_augmentation.Gaussian_blur import GaussianBlur
 def my_transform(isOnline):
     color_jitter = transforms.ColorJitter(0.8, 0.8, 0.8, 0.2)
     my_transforms = transforms.Compose([
-        transforms.RandomResizedCrop(size=(96, 96)),
+        #$transforms.RandomResizedCrop(size=(96, 96)),
         transforms.RandomHorizontalFlip(),
         transforms.RandomApply([color_jitter], p=0.8),
         transforms.RandomGrayscale(p=0.2),
